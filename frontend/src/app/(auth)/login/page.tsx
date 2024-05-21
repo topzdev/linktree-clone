e62c978d95a6edb984e382639dd867f6add2b9ac
topzdev <christianlugod05@gmail.com>
2024-05-21 19:04:11 +0800

    17 upd:form input added

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,32 +1,26 @@
-"use client"
+'use client';
 
-import React from "react";
-import {Label} from "@/components/ui/label";
-import {useForm} from "react-hook-form";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {Credentials, login} from "@/lib/auth";
-import useAppAuth from "@/hooks/useAppAuth";
-import {signIn, useSession} from "next-auth/react";
-import {redirect} from "next/navigation";
-import {toast} from "@/components/ui/use-toast";
-import GoogleLoginButton from "@/components/auth/GoogleLoginButton";
-import FacebookLoginButton from "@/components/auth/FacebookLoginButton";
-import Typography from "@/components/ui/typography";
-import Link from "@/components/ui/link";
-
-
+import React from 'react';
+import {useForm} from 'react-hook-form';
+import {FormInput} from '@/components/ui/input';
+import {Button} from '@/components/ui/button';
+import {Credentials} from '@/lib/auth';
+import useAppAuth from '@/hooks/useAppAuth';
+import {toast} from '@/components/ui/use-toast';
+import GoogleLoginButton from '@/components/auth/GoogleLoginButton';
+import Typography from '@/components/ui/typography';
+import Link from '@/components/ui/link';
 
 type Props = {
-    children?: React.ReactNode
-}
+    children?: React.ReactNode;
+};
 
 const Page = (props: Props) => {
     const {login} = useAppAuth();
-    const {register, handleSubmit, formState} = useForm<Credentials>({
+    const {register, control, handleSubmit, formState} = useForm<Credentials>({
         defaultValues: {
             password: '2559069dev',
-            email: 'christianlugod05@gmail.com'
+            email: 'christianlugod05@gmail.com',
         },
     });
 
@@ -36,47 +30,93 @@ const Page = (props: Props) => {
         } catch (e: any) {
             console.log(e);
             toast({
-                title: "Login Error",
+                title: 'Login Error',
                 description: e?.message,
-                variant: "destructive",
-            })
+                variant: 'destructive',
+            });
         }
     });
 
+    return (
+        <div className='container flex items-center justify-center h-screen'>
+            <div className='grid w-full max-w-sm items-center gap-1.5 mx-auto'>
+                <Typography
+                    className={'text-center'}
+                    variant='h2'
+                    as='h2'
+                >
+                    Welcome Back
+                </Typography>
+                <Typography
+                    foreground='secondary'
+                    className={'text-center'}
+                    variant='p'
+                    as='p'
+                >
+                    Login in to Linktree
+                </Typography>
 
-    return <div className="container flex items-center justify-center h-screen">
-        <div className="grid w-full max-w-sm items-center gap-1.5 mx-auto">
+                <form
+                    className={'flex flex-col gap-y-3 mt-2'}
+                    onSubmit={onSubmit}
+                >
+                    <FormInput
+                        control={control}
+                        name={'email'}
+                        label={'Email'}
+                        id='email'
+                        type='email'
+                        placeholder='Email'
+                    />
+                    <FormInput
+                        control={control}
+                        name={'password'}
+                        label='Password'
+                        id='email'
+                        type='password'
+                        placeholder='Password'
+                    />
+                    <Button
+                        size={'lg'}
+                        rounded
+                        loading={formState.isSubmitting}
+                        disabled={formState.isSubmitting}
+                        type={'submit'}
+                    >
+                        Login
+                    </Button>
+                </form>
 
-            <Typography className={'text-center'} variant="h2" as="h2">Welcome Back</Typography>
-            <Typography foreground="secondary" className={'text-center'} variant="p" as="p">Login in to Linktree</Typography>
+                <Typography
+                    className={'text-center uppercase my-4'}
+                    foreground='secondary'
+                    variant='p'
+                    as='p'
+                >
+                    or
+                </Typography>
 
-            <form className={'flex flex-col gap-y-3 mt-2'} onSubmit={onSubmit}>
-                <div>
-                    <Label htmlFor="email">Email</Label>
-                    <Input  {...register('email')} id="email" type="email" placeholder="Email"/>
+                <div className={'flex flex-col gap-y-2'}>
+                    <GoogleLoginButton/>
                 </div>
-                <div>
-                    <Label htmlFor="email">Password</Label>
-                    <Input   {...register('password')} id="email" type="password" placeholder="Password"/>
-                </div>
-                <Button size={'lg'} rounded loading={formState.isSubmitting} disabled={formState.isSubmitting}
-                        type={'submit'}>Login</Button>
 
-            </form>
-
-            <Typography className={'text-center uppercase my-4'} foreground="secondary" variant="p" as="p">or</Typography>
-            
-            <div className={'flex flex-col gap-y-2'}>
-                <GoogleLoginButton/>
+                <Typography
+                    className={'text-center my-4'}
+                    foreground='secondary'
+                    variant='p'
+                    as='p'
+                >
+                    Don't have an account{' '}
+                    <Link
+                        className='text-blue-500 underline'
+                        href={'/register'}
+                    >
+                        Sign Up
+                    </Link>
+                </Typography>
             </div>
-
-            <Typography className={'text-center my-4'} foreground="secondary" variant="p" as="p">
-                Don't have an account <Link className="text-blue-500 underline" href={'/register'}>Sign Up</Link>
-            </Typography>
-
         </div>
-
-    </div>
-}
+    );
+};
 
 export default Page;
--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -1,25 +1,73 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
+import { Label } from '@/components/ui/label';
+import Typography from '@/components/ui/typography';
+import { Controller } from 'react-hook-form';
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  hint?: string;
+  error?: string | boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ label, error, hint, className, type, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex bg-slate-100 w-full rounded-2xl h-[52px] px-5 py-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+      <div className={'flex flex-col gap-y-1'}>
+        {label && <Label htmlFor={props.id}>{label}</Label>}
+        <input
+          type={type}
+          className={cn(
+            'flex border border-slate-100 bg-slate-100 w-full rounded-2xl h-[52px] px-5 py-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            error && 'bg-red-100 border-red-500 !ring-red-500',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {error && (
+          <Typography
+            variant='detail'
+            className='text-red-500'
+          >
+            {error}
+          </Typography>
         )}
-        ref={ref}
-        {...props}
-      />
-    )
+        {!error && hint && (
+          <Typography
+            variant='detail'
+            foreground='secondary'
+          >
+            {hint}
+          </Typography>
+        )}
+      </div>
+    );
   }
-)
-Input.displayName = "Input"
+);
 
-export { Input }
+type ControlledProp = {
+  control: any;
+  name: string;
+} & InputProps;
+export const FormInput = ({ name, control, ...props }: ControlledProp) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { ref, ...field }, fieldState }) => (
+        <Input
+          {...props}
+          {...field}
+          error={fieldState.error?.message}
+        />
+      )}
+    />
+  );
+};
+
+Input.displayName = 'Input';
+
+export { Input };
