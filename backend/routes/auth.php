4cb679e04dc79bb09ae61196163a3e6c1c89a05e
topzdev <christianlugod05@gmail.com>
2025-05-28 20:30:00 +0800

    docker for laraverl added

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -12,33 +12,32 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->name('register');
+    ->name('register');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest')
-                ->name('login');
-
-Route::post('/oauth', [OauthController::class, 'oauthLogin'])
     ->middleware('guest')
     ->name('login');
 
+Route::post('/oauth', [OauthController::class, 'oauthLogin'])
+    ->middleware('guest')
+    ->name('oauth.login');
+
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->middleware('guest')
-                ->name('password.email');
+    ->middleware('guest')
+    ->name('password.email');
 
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
-                ->middleware('guest')
-                ->name('password.store');
+    ->middleware('guest')
+    ->name('password.store');
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
-                ->name('verification.verify');
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
-                ->name('verification.send');
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
 
 Route::post('/api/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
-                ->name('logout');
-
+    ->middleware('auth')
+    ->name('logout');
